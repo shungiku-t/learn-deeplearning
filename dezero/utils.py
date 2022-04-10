3a0b3bd509c859217a41c8a077755a1322a9cd48
stada526 <s.tada@ntt.com>
2022-04-10 19:07:23 +0900

    add as_variable as helper function

--- dezero/utils.py
+++ dezero/utils.py
@@ -1,10 +1,21 @@
 import numpy as np
 
+from dezero.core import Variable
+
 
 def as_array(x) -> np.ndarray:
     if np.isscalar(x):
         return np.array(x)
     return x
+
+
+def as_variable(obj) -> Variable:
+    if isinstance(obj, Variable):
+        return obj
+    elif isinstance(obj, np.ndarray):
+        return Variable(obj)
+    else:
+        raise TypeError
 
 
 def pprint(var):
